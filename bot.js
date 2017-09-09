b30930c8077f83fb7712b73525b90d40b292d689
jchan601 <justchanabc@yahoo.com>
2017-09-09 00:52:28 -0400

    Add weather
    I hope this works

--- bot.js
+++ bot.js
@@ -8,7 +8,48 @@ var request = require('request');
 
 var version = "2017.09.09.0037",
     botchannel = "355137398897901568", // power plant
-    startup = false;
+    startup = false,
+    weather_apis = ["c042cb323ce03f09", "d33d792d0d281e83", "97817071da18ec7c", "2bace54c80ae0102"],
+    weather_usage = 0;
+    
+function weather(url, channelID) {
+    request.get(url, function(error, response, body) {
+        var json = JSON.parse(body);
+        if ("error" in json.response) {
+            bot.sendMessage({ message: "Error: " + json.response.error.description + ".", to: channelID });
+        } else if ("results" in json.response) {
+            weather_usage++;
+            var res = json.response.results;
+            weather("http://api.wunderground.com/api/" + api + "/conditions" + res[sys.rand(0, res.length)].l + ".json", channelID);
+        } else {
+            var weather = json.current_observation;
+            var out = "Current Weather for " + weather.display_location.full;
+            var degree = function(string) { return string.replace("F", "°F").replace("C", "°C"); };
+            if (weather.display_location.zip != "00000") {
+                out += " (" + weather.display_location.zip + ")";
+            }
+            out += ": ";
+            if (weather.weather) {
+                out += weather.weather + ", ";
+            }
+            out += "Temperature: " + degree(weather.temperature_string) + ", W​ind: " + weather.wind_string;
+            if (!isNaN(weather.precip_today_metric) && +weather.precip_today_metric !== 0) {
+                out += ", Precipation: " + weather.precip_today_string;
+            }
+            if (weather.heat_index_string != "NA") {
+                out += ", Heat Index: " + degree(weather.heat_index_string);
+            }
+            if (weather.windchill_string != "NA") {
+                out += ", Wind Chill: " + degree(weather.windchill_string);
+            }
+            if (weather.feelslike_string != "NA") {
+                out += ", Feels Like: " + degree(weather.feelslike_string);
+            }
+            //out += ", Last Update: " + new Date(weather.observation_epoch * 1000).toGMTString();
+            bot.sendMessage({ message: out, to: channelID });
+        }
+    });
+}
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -55,6 +96,18 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     }
                     bot.sendMessage({ message: "resp: " + Object.keys(response), to: channelID });
                 });
+                break;
+            case "weather":
+				if (data) {
+                    var api = weather_apis[weather_usage % apis.length];
+					var url = "http://api.wunderground.com/api/" + api + "/conditions/q";
+					data = data.split(":");
+                    for (i = 0; i < data.length; i++) {
+                        url += "/" + encodeURIComponent(data[i]);
+                    }
+                    url += ".json";
+					weather(url, channelID);
+				};
                 break;
          }
      }
