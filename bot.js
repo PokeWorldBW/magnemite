6b1a3080edba97f4fdd4335a59a849c31daba106
jchan601 <justchanabc@yahoo.com>
2017-09-09 22:30:30 -0400

    Formatting

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ var auth = require('./auth.json');
 var request = require('request');
 var parser = require('xml2json');
 
-var version = "2017.09.09.2221",
+var version = "2017.09.09.2230",
     owner = "356152143004041218", // DM with Yttrium
     weather_apis = ["c042cb323ce03f09", "d33d792d0d281e83", "97817071da18ec7c", "2bace54c80ae0102"],
     weather_usage = 0,
@@ -156,7 +156,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                                     entry = entry.subsenses.random();
                                 }
                                 var definition = entry.definitions.random();
-                                bot.sendMessage({ message: "`" + data.toUpperCase() + "` (" + partOfSpeech + ") " + definition, to: channelID });
+                                bot.sendMessage({ message: "`" + data.toUpperCase() + "` *" + partOfSpeech.toLowerCase() + "* " + definition, to: channelID });
                             }
                         );
                     }
