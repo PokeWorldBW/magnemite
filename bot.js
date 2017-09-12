f5de026b8473cb100855af605f202553be180632
jchan601 <justchanabc@yahoo.com>
2017-09-12 15:38:31 -0400

    Revert "Add this here?"
    This reverts commit d1a33f0be30508b864949339b199ca0951c4e31e.

--- bot.js
+++ bot.js
@@ -291,33 +291,6 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     }
 });
 
-// Stolen from https://github.com/synicalsyntax/discord.js-heroku#building-a-web-app
-// Prevent app from going to sleep on Heroku
-
-const express = require('express');
-const app = express();
-
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-const port = process.env.PORT || 5000;
-
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// make express look in the `public` directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
-
-// set the home page route
-app.get('/', (request, response) => {
-    // ejs render automatically looks in the views folder
-    response.render('index');
-});
-
-app.listen(port, () => {
-    // will echo 'Our app is running on http://localhost:5000 when run locally'
-    console.log('Our app is running on http://localhost:' + port);
-});
-
 // pings server every 15 minutes to prevent dynos from sleeping
 setInterval(() => {
   request.get("https://magnemite.herokuapp.com");
