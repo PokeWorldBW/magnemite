ab7b91df1554a1648d3859f9536e58c54f077e48
jchan601 <justchanabc@yahoo.com>
2017-09-11 21:51:08 -0400

    Change how this is differentiated

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ var auth = require('./auth.json');
 var request = require('request');
 var parser = require('xml2json');
 
-var version = "2017.09.11.2146",
+var version = "2017.09.11.2150",
     owner = "356152143004041218", // DM with Yttrium
     weather_apis = ["c042cb323ce03f09", "d33d792d0d281e83", "97817071da18ec7c", "2bace54c80ae0102"],
     weather_usage = 0,
@@ -238,7 +238,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 break;
                 case "wiki": case "wikipedia":
                     if (data) {
-                        if (data[0] !== "~") {
+                        if (command === "wiki") {
                             data = data.replace(/\b([A-z]+)/g, function(match) { return match[0].toUpperCase() + match.substring(1).toLowerCase(); });
                         } else {
                             data = data.substring(1);
